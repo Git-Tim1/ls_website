b03fec7b77ac05404de9a15e7e9a0f1995623adf
Git-Tim1 <tim.ostmann@gmx.de>
2023-04-09 00:23:16 +0200

    further development 3

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import LS_Logo from '../files/Logo.jpeg';
 import { AiOutlineMenu, AiOutlineClose, AiFillCaretDown, AiFillCaretRight } from 'react-icons/ai'
 
 const Header = () => {
     const [mobileNavBar, setMobileNavBar] = useState(false)
+    
     return (
     <>
         <header className='w-full h-[5.5rem] drop-shadow bg-white fixed z-20 top-0'>
@@ -54,71 +55,44 @@ const Header = () => {
         <div onClick={() => setMobileNavBar(false)} onTouchStart={() => setMobileNavBar(false)}
             className={`${mobileNavBar ? 'block' : 'hidden'} bg-black fixed opacity-10 z-10 h-full w-full`}>
         </div>
-        {mobileNavBar && <MobileNavBar />}
+        <MobileNavBar showNav={mobileNavBar} changeNavState={() => setMobileNavBar(false)} />
     </>
   )
 }
 
 
-export const MobileNavBar = () => {
+export const MobileNavBar = ({showNav, changeNavState}) => {
     const [showMore, setShowMore] = useState(false)
-    const [showMore2, setShowMore2] = useState(false)
-    return (
-    <div className='z-30 w-full top-[5.5rem] fixed border-t bg-magenta border-white sm:hidden uppercase'>
-        <ul className='m-auto py-2 font-bold text-lg text-white'>
-            <li className='py-3 hover:bg-accent-blue-2'><Link to='/home'>
-            <div className='w-[90%] mx-auto'>Startseite</div></Link></li>
-            <a href="#" onClick={e => {e.preventDefault(); setShowMore(!showMore)}}>
-                <li className='py-3 bg-accent-blue-3 hover:bg-accent-blue-2 border-t border-white'>
-                    <div className='w-[90%] mx-auto'>
-                        Inhalte
-                        {!showMore ? 
-                        <AiFillCaretRight className='float-right accent-blue-1 mt-1 mr-3' /> :
-                        <AiFillCaretDown className='float-right accent-blue-1 mt-1 mr-3' />
-                        }
+    const [showMore1, setShowMore1] = useState(false)
+    return(
+        <div className={`absolute w-full h-full bg-gradient-to-tr from-magenta to-[#5800FF] z-[100] top-0 ${showNav ? 'left-0 sm:left-[-120%]' : 'left-[-120%]'} duration-200`}>
+            <div className='h-[5.5rem] w-full border-b border-white'>
+                <div className='w-[90%] h-full mx-auto'>
+                    <div className='flex items-center h-full float-right'>
+                        <a className='float-right my-auto w-auto flex' href="#" onClick={e => {e.preventDefault(); changeNavState()} }>
+                            <AiOutlineClose size={25} className='sm:hidden my-auto text-white' />
+                        </a>
                     </div>
-                </li>
-            </a>
-            {showMore && <div className='border-t border-white bg-decent-blue font-medium bg-white text-black'>
-                 <ul>
-                    <li className='border-b border-gray-400'><Link to='/unsere-positionen'><div className='w-[85%] mx-auto pb-3 pt-4'>
-                        Unsere Positionen
-                    </div></Link></li>
-                    <li><Link to='/news'><div className='w-[85%] mx-auto pt-3 pb-4'>
-                        Neuigkeiten
-                    </div></Link></li>
-                </ul>
-            </div>}
-            <a href="#" onClick={e => {e.preventDefault(); setShowMore2(!showMore2)}}>
-                <li className='py-3 bg-accent-blue-3 hover:bg-accent-blue-2 border-t border-white'>
-                    <div className='w-[90%] mx-auto'>
-                        Über uns 
-                        {!showMore2 ? 
-                        <AiFillCaretRight className='float-right accent-blue-1 mt-1 mr-3' /> :
-                        <AiFillCaretDown className='float-right accent-blue-1 mt-1 mr-3' />
-                        }
+                </div>
+            </div>
+            <div className='w-full'>
+                <div className='w-[85%] mt-8 text-3xl font-black text-white mx-auto'>
+                    <a className='hover:text-yellow-300 hover:ml-1 duration-200 inline-block w-full' href="#" onClick={() => setShowMore(!showMore)}>Inhalte</a>
+                    <div className={`border bg-white text-lg text-black mt-1 py-3 rounded ${showMore ? 'hidden' : 'block'}`}>
+                        <Link onClick={() => changeNavState()} to="/positionen" className='hover:text-magenta ml-6 hover:ml-7 mb-1 font-black w-full inline-block duration-200'>Positionen</Link>
+                        <Link onClick={() => changeNavState()} to="/news" className='hover:text-magenta  ml-6 hover:ml-7 mt-1 font-black w-full inline-block duration-200'>Neuigkeiten</Link>
                     </div>
-                </li>
-            </a>
-            {showMore2 && <div className='border-t border-white bg-white text-black font-medium'>
-                 <ul>
-                    <li className='border-b border-gray-400'><Link to='/landesvorstand'><div className='w-[85%] mx-auto pb-3 pt-4'>
-                        Landesvorstand
-                    </div></Link></li>
-                    <li><Link to='/veranstaltungen'><div className='w-[85%] mx-auto pt-3 pb-4'>
-                        Veranstaltungen
-                    </div></Link></li>
-                </ul>
-            </div>}
-            <li className='py-3 border-t border-white  hover:bg-accent-blue-2'>
-                <Link to='/mitglied-werden'>
-                    <div className='w-[90%] mx-auto'>Mitglied werden</div>
-                </Link>
-            </li>
-            <li className='py-3 border-t border-white bg-accent-blue-3  hover:bg-accent-blue-2'><Link to='/kontakt'><div className='w-[90%] mx-auto'>Kontakt</div></Link></li>
-        </ul>
-    </div>
+                    <a className='hover:text-yellow-300 hover:ml-1 duration-200 inline-block w-full mt-4' href="#" onClick={() => setShowMore1(!showMore1)}>Über uns</a>
+                    <div className={`border bg-white text-lg text-black mt-1 py-3 rounded ${showMore1 ? 'hidden' : 'block'}`} >
+                        <Link onClick={() => changeNavState()} to="/landesvorstand" className='hover:text-magenta ml-6 hover:ml-7 pb-1 font-black w-full inline-block duration-200'>Landesvorstand</Link>
+                        <Link onClick={() => changeNavState()} to="/events" className='hover:text-magenta ml-6 hover:ml-7 pt-1 font-black w-full inline-block duration-200'>Events</Link>
+                    </div>
+                    <Link className='hover:text-yellow-300 hover:ml-1 inline-block w-full duration-200 mt-4' to="/kontakt">Kontakt</Link>
+                    <Link className='hover:text-yellow-300 hover:ml-1 inline-block w-full duration-200 mt-4' to="/mitglied-werden">Mitglied werden</Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Header
+export default Header;
